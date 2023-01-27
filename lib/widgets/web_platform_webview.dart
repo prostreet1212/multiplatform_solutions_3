b3034b9900cc66ca5d40a6ad4c076cb7855a6be9
prostreet1212@gmail.com <ktndlf,tcfyu2020>
2023-01-27 10:59:50 +0300

    №3

--- lib/widgets/web_platform_webview.dart
+++ lib/widgets/web_platform_webview.dart
@@ -1,6 +1,4 @@
 import "package:universal_html/html.dart";
-
-//import 'dart:html';
 import 'dart:math';
 import 'package:flutter/material.dart';
 import 'dart:ui' as ui;
@@ -8,42 +6,6 @@ import '../app_platform.dart';
 import 'find_panel.dart';
 
 Widget webView() => const WebPlatformWebView();
-/*
-class WebPlatformWebView extends StatelessWidget {
-   WebPlatformWebView({Key? key}) : super(key: key);
-
-  String id='';
-  void loadUrl(String url) {
-    id = Random().nextInt(1000).toString();
-    // ignore: undefined_prefixed_name
-    ui.platformViewRegistry.registerViewFactory(
-        id, (int viewId) => IFrameElement()..src = url);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Expanded(
-          flex: 12,
-          child: HtmlElementView(viewType: id),
-          /*child: InAppWebView(
-            initialUrlRequest: URLRequest(url: Uri.parse('https://kdrc.ru')),),*/
-        ),
-        Container(color: Colors.black45, height: 1),
-        FindPanel(load: loadUrl),
-        Text(
-          'APPLICATION RUNNING ON ${AppPlatform.platform.toUpperCase()}',
-          style: const TextStyle(fontWeight: FontWeight.bold),
-        ),
-        const SizedBox(
-          height: 8,
-        )
-      ],
-    );
-  }
-}
-*/
 
 class WebPlatformWebView extends StatefulWidget {
   const WebPlatformWebView({Key? key}) : super(key: key);
